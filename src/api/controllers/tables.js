c68212a425c4fc8c5c930cd5168568b24b5cded4
manologonz <manologonz203@gmail.com>
2020-11-01 18:24:40 -0600

    ADD: list of all tables

--- src/api/controllers/tables.js
+++ src/api/controllers/tables.js
@@ -1,12 +1,31 @@
-
 // models
 const Table = require('../models/table');
+
+// helpers
+const helpers = require('../utils/helpers')
 
 // validators
 const {validationResult} = require('express-validator');
 
+// pagination
+const paginate = require('express-validator')
+
 exports.listTables = (req, res, next) => {
-    res.json({detail: 'Not supported yet'});
+    try {
+        const tablesCount = await Table.find({}).countDocuments({});
+        const results = await Table.find({}, {__v: 0}).limit(req.query.limit).skip(req.skip).lean().exec();
+        const pageCount = Math.ceil(tablesCount/ req.query.limit);
+        res.status(200).json({
+            next: paginate.hasNextPages(req)(pageCount),
+            results
+        });
+    } catch (err) {
+        next(helpers.handleError(err));
+    }
+}
+
+exports.createTable= (req, res, next) => {
+    res.json({'detal': 'Not suported yet'})
 }
 
 exports.updateTable = (req, res, next) => {
--- src/api/routes/tables.js
+++ src/api/routes/tables.js
@@ -11,17 +11,19 @@ const {
     updateTable, 
     tableDetail,
     deleteTable,
-    authenticateClient
+    authenticateClient,
+    createTable
 } = require('../controllers/tables')
 
 // permissions
 const {isAuthenticated, isAdmin} = require('../utils/permissions');
 const basePermissions = [isAuthenticated, isAdmin];
 
-router.post('/:idSubsidiary', basePermissions, listTables);
-router.put('/:idSubsidiary/:idTable', basePermissions, updateTable);
-router.get('/:idSubsidiary/:idTable', basePermissions, tableDetail);
-router.delete('/:idSubsidiary/:idTable', basePermissions, deleteTable);
-router.post('/:idSubsidiary/:authCode/authenticate-client', authenticateClient);
+router.post('/:idSubsidiary/tables', basePermissions, createTable);
+router.get('/:idSubsidiary/tables', basePermissions, listTables);
+router.put('/:idSubsidiary/tables/:idTable', basePermissions, updateTable);
+router.get('/:idSubsidiary/tables/:idTable', basePermissions, tableDetail);
+router.delete('/:idSubsidiary/tables/:idTable', basePermissions, deleteTable);
+router.post('/:idSubsidiary/tables/:authCode/authenticate-client', authenticateClient);
 
 module.exports = router;
